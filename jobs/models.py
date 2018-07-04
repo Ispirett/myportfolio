02989b563cf664ce83d865cfc2ab622241ffed80
Ispire <ispirett@gmail.com>
2018-07-04 04:01:16 -0700

    Added jobs to page
    altered table and added title
    added for loop to html div

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=40)
     summary = models.CharField(max_length=200)
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')
+
+    my_jobs = Job.objects
+    return render(request, 'jobs/home.html', {'my_jobs': my_jobs})
